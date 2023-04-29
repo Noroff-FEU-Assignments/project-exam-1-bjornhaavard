02aee85a6bada9d9ffa8a2a2bb206fe3136d5c31
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-04-29 16:39:53 +0200

    validation update

--- js/views/posts/displayPostList.js
+++ js/views/posts/displayPostList.js
@@ -1,16 +1,14 @@
 import { getPosts } from "../../api/posts.js";
 
-
 export default async function displayPostList() {
-  
-  let loader = document.querySelector(".lds-ripple")
+  let loader = document.querySelector(".lds-ripple");
   const posts = await getPosts();
   const container = document.querySelector("#postContainer");
   const morePosts = 10;
   const firstPosts = posts.slice(0, 10);
   renderPosts(firstPosts, "#postContainer");
-  if(posts) {
-    loader.style.display = "none"
+  if (posts) {
+    loader.style.display = "none";
   }
   if (posts.length > morePosts) {
     const readMoreButton = document.createElement("button");
@@ -23,29 +21,26 @@ export default async function displayPostList() {
         const blogPost = createPost(post);
         container.appendChild(blogPost);
       });
- 
+
       readMoreButton.style.display = "none";
     });
     container.appendChild(readMoreButton);
   }
-
 }
 
-
 export function renderPosts(posts, selector) {
-  const container = document.querySelector(selector)
+  const container = document.querySelector(selector);
   posts.forEach((post) => {
     const blogPost = createPost(post);
     container.appendChild(blogPost);
+    console.log(posts);
   });
 }
 
-
 export function createPost(post) {
-
   const { id, title, content } = post;
   const div = document.createElement("a");
-  const heading = document.createElement("h2");
+  const heading = document.createElement("h3");
   const img = getImageFromContent(content.rendered);
   const imageContainer = document.createElement("div");
   div.classList.add("post-card");
@@ -58,52 +53,25 @@ export function createPost(post) {
 }
 
 export function getImageFromContent(html) {
-  
   const parser = new DOMParser();
   const parsedDocument = parser.parseFromString(html, "text/html");
   const img = parsedDocument.querySelector("img");
   return img.src;
 }
 
-
-const huntButton = document.querySelector("#hunt")
-const trainButton = document.querySelector("#train")
-const matchButton = document.querySelector("#match")
-
-// const handleClick = (event) => {
-//   const button = event.target.dataset.id
-
-
-// };
-// filterButtons.forEach(button => {
-//   button.addEventListener("click", handleClick)
+// const searchButton = document.querySelector(".search-button")
+// searchButton.addEventListener("click", (container = document.querySelector(".")) =>  {
+//   const searchInput = document.querySelector("#search-input").value;
+//   const newBlogPost = `?search=${searchInput}`
+//   container = ""
+//   container.appendChild()
+//   displayPostList(newBlogPost)
+//   console.log(searchInput)
 // });
 
-// buttons.forEach.addEventListener("click", (event)  => {
-//     const { id } = event.target.dataset
+// function searchPosts (posts) {
+//   getPosts()
+//   console.log(posts)
+// }
 
-//     newPosts = { id }
-
-//   console.log("test")
-// });
-
-// huntButton.addEventListener("click", (event)  => {
-//   renderPosts()
-//   const id = event.target.dataset.id
-//   const newBlogPost = blogPost
-//   const url = `newBlogPost${newBlogPost}`;
-//   blogPost.appendChild(url)
-//   console.log(id)
-// });
-
-// trainButton.addEventListener("click", ()  => {
-  
-//   console.log("train button")
-// });
-
-// matchButton.addEventListener("click", ()  => {
-  
-//   console.log("match button")
-// });
-
-
+// searchPosts()
